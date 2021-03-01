cd2b2dee63fa6a408d6d22d3d51fc7f1be9df071
sune <sune1309@edu.easj.dk>
2021-03-01 15:46:30 +0100

    hej

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" media="all" rel="stylesheet" type="text/css">
+    <title>hjemmeside</title>
+</head>
+<body>
+    <p>oh yeah it all comming together</p>
+</body>
+</html>
+
 <?php
 echo "hej<br>";
 echo "fuck<br>";
